c3f5bb36994972342bf931cab139d7177a7056cb
MrGeet <aldarbatomunkuev@gmail.com>
2020-11-01 16:56:40 +1300

    Added charts to the page

--- src/components/Content/Percentage.js
+++ src/components/Content/Percentage.js
@@ -1,12 +1,79 @@
 import React from "react";
 import styled from "styled-components";
-import './BoxStyle.css';
+import "./BoxStyle.css";
+import { Pie } from "react-chartjs-2";
 
 const Styles = styled.div`
     width: 50%;
     display: inline-block;
 `;
+const Chart = styled.div`
+    margin: 30px;
+    width: 45%;
+    min-height: 50%;
+    display: flex;
+    position: absolute;
+`;
 
-export const Percentage = () => (
-    <div className='half box-style'>Percentage</div>
-);
+const Title = styled.div`
+    font-size: 15px;
+    font-weight: 300;
+    padding: 30px;
+    width: 100%;
+`;
+
+export const Percentage = () => {
+    const data1 = {
+        labels: ["Gala", "Envy", "Royal"],
+        datasets: [
+            {
+                label: "Production",
+                data: [41, 27, 32],
+                // borderColor: ["#E5462C"],
+                backgroundColor: ["#38C976", "#FC7D58", "#FDCC36"],
+            },
+        ],
+    };
+    const data2 = {
+        labels: ["Gala", "Envy", "Royal"],
+        datasets: [
+            {
+                label: "Cost",
+                data: [59, 30, 11],
+                // borderColor: ["#E5462C"],
+                backgroundColor: ["#38C976", "#FC7D58", "#FDCC36"],
+            },
+        ],
+    };
+
+    const options1={
+        maintainAspectRatio: false,
+
+        legend:{
+            display:false,
+        }
+    }
+
+    const options2 ={
+        maintainAspectRatio: false,
+
+        legend:{
+            position: 'right',
+            align: 'center',
+
+            labels:{
+                // shape: 'circle',
+            }
+        }
+    }
+
+    return (
+        <div className="half box-style">
+            <Title>PERCENTAGE</Title>
+            <Chart>
+                <Pie data={data1} options={options1}></Pie>
+                <Pie data={data2} options={options2}></Pie>
+            </Chart>
+        </div>
+    );
+};
--- src/components/Content/SizeDistribution.js
+++ src/components/Content/SizeDistribution.js
@@ -1,12 +1,68 @@
 import React from "react";
 import styled from "styled-components";
-import './BoxStyle.css';
+import "./BoxStyle.css";
+import { Bar } from "react-chartjs-2";
 
 const Styles = styled.div`
     width: 50%;
     display: inline-block;
 `;
 
-export const SizeDistribution = () => (
-    <div className='box-style'>SizeDistribution</div>
-);
+const Title = styled.div`
+    font-size: 15px;
+    font-weight: 300;
+    padding: 30px;
+    width: 100%;
+`;
+
+const TempStyle = styled.div`
+    padding: 30px 20px;
+    /* width: 100%; */
+    position: relative;
+    right: 40%;
+`;
+
+const Chart = styled.div`
+    width: 70%;
+    min-height: 70%;
+    /* height: 30%; */
+    display: flex;
+    /* position: absolute; */
+    /* justify-content: center; */
+    /* align-items: center; */
+    /* top: 10%; */
+    margin: auto;
+    /* overflow: auto; */
+`;
+
+export const SizeDistribution = () => {
+    const data = {
+        labels: [34, 48, 56, 88, 102, 113, 125, 135, 150, 163],
+        datasets: [
+            {
+                label: "Total Apples",
+                data: [2, 22, 34, 123, 450, 700, 423, 340, 230, 123],
+                borderColor: ["#E5462C"],
+                backgroundColor: ["rgba(229,70,44,0.2"],
+            },
+        ],
+    };
+
+    const options = {
+        maintainAspectRatio: false,
+        responsive: true
+    };
+
+    return (
+        <div className="box-style">
+            <div className="flex-container">
+                <Title>SIZE DISTRIBUTION</Title>
+                <TempStyle>(Varieties)</TempStyle>
+                <TempStyle>(Orchards)</TempStyle>
+            </div>
+                <Chart>
+                    <Bar data={data} options={options}></Bar>
+                </Chart>
+        </div>
+    );
+};
